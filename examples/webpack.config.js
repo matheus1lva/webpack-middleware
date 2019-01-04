0198271512282077f0afe626be4c398b5e2e0913
PlayMa256 <matheus.g.silva@hotmail.com>
2019-01-04 01:41:05 -0200

    websocket wired up, ready to broadcast hmr updates

--- examples/webpack.config.js
+++ examples/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: ['./app.js', 'webpack-hot-middleware/client'],
+  entry: './app.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
